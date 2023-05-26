290a5b8b71cbb30ceaf9409954c78fe8b1f0465b
Jan Langguth <jl@delife.eu>
2023-05-26 08:51:40 +0200

    New: Vue Komponenten erstellt.

---
+++ resources/js/vueConverter/newTransactionConverter.vue
@@ -0,0 +1,25 @@
+<template>
+  <HelloWorld2 />
+</template>
+
+<script>
+import HelloWorld2 from '../components/newTransaction.vue'
+
+export default {
+  name: 'newTransaction',
+  components: {
+    HelloWorld2
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
